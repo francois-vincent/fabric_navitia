5016b0c67133da04eb6b5ffb13c55d46cceb276f
François Vincent <francois.vincent01@gmail.com>
2015-09-24 15:15:54 +0200

    fix temp file management and owner/privileges

--- fabfile/component/kraken.py
+++ fabfile/component/kraken.py
@@ -166,12 +166,10 @@ def swap_data_nav(instance):
     """
     temp_target = instance.temp_target_lz4_file
     plain_target = instance.plain_target_lz4_file
-    if files.is_file(temp_target) and files.is_file(plain_target) and \
-       files.getmtime(temp_target) > files.getmtime(plain_target):
-        swap_temp = os.path.join(os.path.dirname(temp_target), 'x')
-        files.move(plain_target, swap_temp)
-        files.move(temp_target, plain_target)
-        files.move(swap_temp, temp_target)
+    if files.is_file(temp_target):
+        sudo('chown www-data:www-data {}'.format(temp_target))
+        sudo('chmod 666 {}'.format(temp_target))
+        files.copy(temp_target, plain_target)
 
 @task
 @roles('eng')
